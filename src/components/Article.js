46754456082ee7e5b2544cc198e3d3ed8891c748
LittleLebowski <o.f.atas@gmail.com>
2022-12-06 17:18:39 +0300

    Completed Assignment

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,13 +1,23 @@
 import React from "react";
 
-const Article = ({ post }) => {
-  const { date = "January 1, 1970", minutes, preview, title } = post;
+// const Article = ({ post }) => {
+//   const { date = "January 1, 1970", minutes, preview, title } = post;
 
+//   return (
+//     <article>
+//       <h3>{title}</h3>
+//       {/*<small>{date ? date : "January 1, 1970"}</small>*/}
+//       {/*<small>{date || "January 1, 1970"}</small>*/}
+//       <small>{date}</small>
+//       <p>{preview}</p>
+//     </article>
+//   );
+// };
+
+const Article = ({ title, date = "January 1, 1970", preview }) => {
   return (
     <article>
       <h3>{title}</h3>
-      {/*<small>{date ? date : "January 1, 1970"}</small>*/}
-      {/*<small>{date || "January 1, 1970"}</small>*/}
       <small>{date}</small>
       <p>{preview}</p>
     </article>
--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -2,14 +2,23 @@ import React from "react";
 import Article from "./Article";
 
 const ArticleList = ({ posts }) => {
-  return (
-    <main>
-      {posts.map((post) => {
-        console.log(post, "ilk");
-        return <Article key={post.id} post={post} />;
-      })}
-    </main>
-  );
+  const articlePosts = posts.map((post) => (
+    <Article
+      key={post.id}
+      title={post.title}
+      date={post.date}
+      preview={post.preview}
+    />
+  ));
+  return <main>{articlePosts}</main>;
+  // return (
+  //   <main>
+  //     {posts.map((post) => {
+  //       console.log(post, "ilk");
+  //       return <Article key={post.id} post={post} />;
+  //     })}
+  //   </main>
+  // );
 };
 
 export default ArticleList;
